02c2336b373ab22e3af0236c0d107d354b071328
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-11-08 13:41:52 +0200

    6. Implement Counter Bloc by emitting a State for every Event

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,13 +1,22 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
+  CounterBloc() : super(CounterState.initial()) {
+    on<IncrementCounterEvent>((event, emit) {
+      emit(state.copyWith(counter: state.counter + 1));
     });
+
+    on<DecrementCounterEvent>(_decrementCounter);
+  }
+
+  void _decrementCounter(
+    DecrementCounterEvent event,
+    Emitter<CounterState> emit,
+  ) {
+    emit(state.copyWith(counter: state.counter - 1));
   }
 }
