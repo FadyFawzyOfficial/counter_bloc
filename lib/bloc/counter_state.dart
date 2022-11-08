8d4411112ecaa8f2eea45d590f3d0e03638490e6
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-11-08 12:32:46 +0200

    4. Build Counter State copied from Counter Cubit App

--- lib/bloc/counter_state.dart
+++ lib/bloc/counter_state.dart
@@ -1,10 +1,27 @@
 part of 'counter_bloc.dart';
 
-abstract class CounterState extends Equatable {
-  const CounterState();
-  
-  @override
-  List<Object> get props => [];
-}
+class CounterState extends Equatable {
+  final int counter;
 
-class CounterInitial extends CounterState {}
+  const CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return const CounterState(counter: 0);
+  }
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+
+  @override
+  List<Object?> get props => [counter];
+
+  @override
+  String toString() => 'CounterState(counter: $counter)';
+}
