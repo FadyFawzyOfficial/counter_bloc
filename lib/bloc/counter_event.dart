2825a3abad7ecaac8622a484c4db5a268619df5a
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2022-11-08 13:32:13 +0200

    5. Add Counter Events (Increment & Decrement Events)

--- lib/bloc/counter_event.dart
+++ lib/bloc/counter_event.dart
@@ -6,3 +6,7 @@ abstract class CounterEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class IncrementCounterEvent extends CounterEvent {}
+
+class DecrementCounterEvent extends CounterEvent {}
